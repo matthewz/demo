49354e6612d0ff729f07349e1caa103c298e8125
Matt Zeller <matt.zeller@me.com>
2017-04-05 11:37:50 -0400

    checking name and email

---
+++ PS/function.psm1
@@ -0,0 +1,47 @@
+#
+function Get-SystemStatus
+{
+param (
+        [string[]] $Servers = "localhost"
+      , [string] $Env
+)
+
+Write-Output "Server(s): $Servers"
+Write-Host "Debug: Debug"
+
+foreach ($Server in $Servers)
+{ 
+   $Command = "Test-Connection -ComputerName $Server -Count 2 -Quiet"
+   if ($Debug -eq $True)
+   {
+      Write-Output "CMD=$Command"
+   }
+   else
+   {
+      Write-Output "CMD=$Command"
+      Write-Output "Checking...$Server"
+      $Result = Invoke-Expression $Command
+      Write-Output "Result = $Result"
+   }
+   If ($Result -eq $True) 
+   {
+      Write-Output "Server: $Server is up."
+   }
+   ElseIf ($Result -eq $False) 
+   {
+      Write-Output "Server: $Server is down."
+   }
+   Else
+   {
+      if ($Debug -eq $True)
+      {
+         Write-Output "Running in Debug mode"
+      }
+      Else
+      {
+         Write-Output "Unexpected result from: $Command"
+      }
+   }
+}
+
+}
---
+++ PS/run.ps1
@@ -0,0 +1,21 @@
+param (
+        [string[]] $Servers = "localhost"
+      , [string] $Env = "dev"
+      , [bool] $Debug = $False
+)
+
+$Global:Debug=$Debug
+
+$Scriptpath =(Split-Path -parent -Path $MyInvocation.MyCommand.path)
+
+Write-Host "Base Directory: $scriptpath"
+
+Write-Host "Server(s): $Servers"
+
+Write-Host "Environment(s): $Env"
+
+Write-Host "Debug: $Global:Debug"
+ 
+Import-Module $scriptpath\function -force
+
+Get-SystemStatus -Servers $Servers
