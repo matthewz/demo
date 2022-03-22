a58cd7d2766d7daf76b7026d03c99dfe3e07f135
Matt <51680+matthewz@users.noreply.github.com>
2022-03-22 17:48:33 -0400

    Update init.py

--- PY/init.py
+++ PY/init.py
@@ -22,9 +22,14 @@ def parse_args():
                       , help="debug switch - if --debug is found, diagnostics will be turned on"
                       )
    parser.add_argument("--app"
-                      , default="app"
+                      , default="ALL"
                       , nargs="?"
-                      , help="app = app(default), abc, tla, fcb, googl, ibm, aol, any, all"
+                      , help="app = ALL(default) , app, abc, tla, fcb, googl, ibm, aol, any, all"
+                      )
+   parser.add_argument("--env"
+                      , default="ALL"
+                      , nargs="?"
+                      , help="env = ALL(default), qal, pte, stg, prd, ALL"
                       )
    parser.add_argument("--type"
                       , default="java"
@@ -34,7 +39,7 @@ def parse_args():
    parser.add_argument("--action"
                       , default="info"
                       , nargs="?"
-                      , help="action = info(default), build, package, ship, install, deploy, verify"
+                      , help="action = info(default), build, package, ship, install, deploy, verify, compute"
                       )
    parser.add_argument("--file"
                       , default=""
@@ -42,34 +47,34 @@ def parse_args():
                       , help="file = full path to file containing further arguments or data"
                       )
    parser.add_argument("--proj"
-                      , default="app-default-dev"
+                      , default="ALL"
                       , nargs="?"
-                      , help="proj = app-default-dev(default), [GCP Project Name]"
+                      , help="proj = ALL(default), [GCP Project Name]...e.g. " + args.app_project_prefix + "${ENV}"
                       )
    parser.add_argument("--shared_project"
-                      , default="org-shared-env"
+                      , default=args.dns_project_prefix 
                       , nargs="?"
-                      , help="shared_project = org-shared-env(default), [GCP Project Name containing DNS Zone]"
+                      , help="shared_project = " + args.dns_project_prefix + "${env}(default), [GCP Project Name containing DNS Zone]"
                       )
    parser.add_argument("--images_project"
-                      , default="org-images-project"
+                      , default=args.img_project 
                       , nargs="?"
                       , help="images_project = team-images-project(default), [GCP Project Name containing base images]"
                       )
    parser.add_argument("--zone"
-                      , default="us-east1-a"
+                      , default="ALL"
                       , nargs="?"
-                      , help="zone = use-east1-a(default) GCE zone to list / modify"
+                      , help="zone = ALL(default) GCE zone to list / modify...e.g. us-west2-a"
                       )
    parser.add_argument("--dns_zone"
-                      , default="internal.zone"
+                      , default=args.dns_zone_suffix
                       , nargs="?"
-                      , help="dns_zone = internal.zone where DNS Records are stored"
+                      , help="dns_zone = internal.zone where DNS Records are stored...e.g. ${ENV}" +  args.dns_zone_suffix 
                       )
    parser.add_argument("--region"
-                      , default="us-east1"
+                      , default="ALL"
                       , nargs="?"
-                      , help="region = use-east1(default) GCE zone to list / modify"
+                      , help="region = ALL(default) GCE zone to list / modify...e.g. us-east1"
                       )   
    parser.add_argument("--acct"
                       , default=""
@@ -104,4 +109,13 @@ def validate_args():
    else:
       print("ERROR - Cannot set log level to:" + " " + args.cmd_line_args.log_level + "" + "(" + str(log_level_nbr) + ")")
       return False
+   if args.cmd_line_args.shared_project == args.dns_project_prefix:
+      args.cmd_line_args.shared_project = args.dns_project_prefix + args.cmd_line_args.env
+   if args.cmd_line_args.dns_zone == args.dns_zone_suffix:
+      args.cmd_line_args.dns_zone = args.cmd_line_args.env + args.cmd_line_args.dns_zone
+   if args.cmd_line_args.app == "ALL":
+      args.app_filter = "*"
+   else: 
+      args.app_filter = args.cmd_line_args.app + "*"
    return True 
+# TODO: scan DNS for both shared-$ENV and shared-ext-$ENV, set all env as global variables here, and, handle nonprod vs. prod as well as the six, dev, qal, pte, stg, prd, and, what about app filter(does not work for images et al)> , do we need some more arguments...26 maybe?, add gcloud cmd as an arguement to --action, maybe make --action a list of selections...if not already , why does from module import module blah blah have to be done?, ensure all arg defaults are correct and synch. with constances in pvars.py 
